bec6db2b2ec179e46d59f448c9b68633a08f227d
Akshat <akshatthakuransh@gmail.com>
2024-05-27 13:05:50 +0530

    added_footer

--- ncc/src/App.js
+++ ncc/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from "./components/Home/Home.js";
 // import Navbar from "./components/Navbar/Navbar.js";
+import Footer from "./components/Footer/footer.js"
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
           <Route exact={true} path='/' element={<Home />} />
         </Routes>
         {/* <Footer /> */}
+        <Routes>
+          <Route exact={true} path='/' element={<Footer />} />
+        </Routes>
       </Router>
     </div>
   );
