40ff151303ace3ade66ad2af2a8c3b5f77c50f75
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-06-03 16:03:41 +0530

    update depreciated queryselector with modern useRef

--- ncc/src/components/AnnualReport/AnnualReport.js
+++ ncc/src/components/AnnualReport/AnnualReport.js
@@ -1,48 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react';
 import './AnnualReport.css';
 
 export default function AnnualReport() {
-
-    useEffect(() => {
-        document.title = 'Annual Report – NCC';
-
-        if (reports.length>4){
-            const prev = document.querySelector('.prev');
-            const next = document.querySelector('.next');
-
-            const track = document.querySelector('.track');
-            let cards = document.querySelectorAll('.card-container');
-            let cardWidth = document.querySelector('.card-container').offsetWidth;
-
-            let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-
-            window.addEventListener('resize', () => {
-                cardWidth = document.querySelector('.card-container').offsetWidth;
-                carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-            })
-
-            let index = 0;
-
-            next.addEventListener('click', () => {
-                index++;
-                prev.classList.add('show');
-                track.style.transform = `translateX(-${index * cardWidth}px)`;
-
-                if ((cards.length * cardWidth - ((index + 1) * cardWidth)) < carouselWidth) {
-                    next.classList.add('hide');
-                }
-            })
-
-            prev.addEventListener('click', () => {
-                index--;
-                next.classList.remove('hide');
-                if (index === 0) {
-                    prev.classList.remove('show');
-                }
-                track.style.transform = `translateX(-${index * cardWidth}px)`;
-            })
-        }
-    }, []);
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
+    const trackRef = useRef(null);
+    const cardContainerRef = useRef(null);
+    const carouselContainerRef = useRef(null);
 
     const reports = [
         { link: "https://drive.google.com/file/d/1kAgogATQnVMk3ikKk5sFv3QoAVrTZ-vW/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2021.jpg", altText: "annual report 20-21" },
@@ -50,21 +14,75 @@ export default function AnnualReport() {
         // { link: "https://drive.google.com/file/d/1kAgogATQnVMk3ikKk5sFv3QoAVrTZ-vW/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2021.jpg", altText: "annual report 20-21" },
         // { link: "https://drive.google.com/file/d/1b9w3FArFXU7Jj4sLXbRKhiCy7sEb2xTA/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2020.jpg", altText: "annual report 19-20" },
         // { link: "https://drive.google.com/file/d/1kAgogATQnVMk3ikKk5sFv3QoAVrTZ-vW/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2021.jpg", altText: "annual report 20-21" },
+        // { link: "https://drive.google.com/file/d/1b9w3FArFXU7Jj4sLXbRKhiCy7sEb2xTA/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2020.jpg", altText: "annual report 19-20" },
+        // { link: "https://drive.google.com/file/d/1kAgogATQnVMk3ikKk5sFv3QoAVrTZ-vW/view?usp=sharing", imgSrc: "./images/annualreport/annlrp2021.jpg", altText: "annual report 20-21" },
     ];
+
+    useEffect(() => {
+        document.title = 'Annual Report – NCC';
+
+        if (reports.length > 4) {
+            const prev = prevRef.current;
+            const next = nextRef.current;
+            const track = trackRef.current;
+            const cardContainer = cardContainerRef.current;
+            const carouselContainer = carouselContainerRef.current;
+            let cardWidth = cardContainer.offsetWidth;
+            let carouselWidth = carouselContainer.offsetWidth;
+
+            const handleResize = () => {
+                cardWidth = cardContainer.offsetWidth;
+                carouselWidth = carouselContainer.offsetWidth;
+            };
+
+            window.addEventListener('resize', handleResize);
+
+            let index = 0;
+
+            const handleNext = () => {
+                index++;
+                prev.classList.add('show');
+                track.style.transform = `translateX(-${index * cardWidth}px)`;
+
+                if ((reports.length * cardWidth - ((index) * cardWidth)) < carouselWidth) {
+                    next.classList.add('hide');
+                }
+            };
+
+            const handlePrev = () => {
+                index--;
+                next.classList.remove('hide');
+                if (index === 0) {
+                    prev.classList.remove('show');
+                }
+                track.style.transform = `translateX(-${index * cardWidth}px)`;
+            };
+
+            next.addEventListener('click', handleNext);
+            prev.addEventListener('click', handlePrev);
+
+            return () => {
+                next.removeEventListener('click', handleNext);
+                prev.removeEventListener('click', handlePrev);
+                window.removeEventListener('resize', handleResize);
+            };
+        }
+    }, [reports]);
 
     return (
         <div className='container'>
-            <p class="heading">Annual Reports</p>
-            <div class="reports">
+            <p className="heading">Annual Reports</p>
+            <div className="reports">
                 <div className='curr-year'>
-                    <div class="Report-box">
-                        <a href="https://drive.google.com/file/d/1XQC6-aUcZG6igPKWHH0zacx0M0uEPU7l/view?usp=drive_link" target="_blank">
-                            <img src="./images/annualreport/annlrp2023.jpg" alt="annual report 23-24" /></a>
+                    <div className="Report-box">
+                        <a href="https://drive.google.com/file/d/1XQC6-aUcZG6igPKWHH0zacx0M0uEPU7l/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                            <img src="./images/annualreport/annlrp2023.jpg" alt="annual report 23-24" />
+                        </a>
                     </div>
                 </div>
-                <div class="prev-year">
+                <div className="prev-year">
                     {reports.length < 5 ?
-                        <div className={`d-flex ${window.innerWidth<750 ? 'flex-column' : ''} justify-content-evenly`}>
+                        <div className={`d-flex ${window.innerWidth < 750 ? 'flex-column' : ''} justify-content-evenly`}>
                             {reports.map((report, index) => (
                                 <div className="card-container" key={index}>
                                     <div className="card">
@@ -77,11 +95,11 @@ export default function AnnualReport() {
                                 </div>
                             ))}
                         </div> :
-                        <div class="carousel-container">
-                            <div class="carousel-inner">
-                                <div class="track">
+                        <div className="carousel-container" ref={carouselContainerRef}>
+                            <div className="carousel-inner">
+                                <div className="track" ref={trackRef}>
                                     {reports.map((report, index) => (
-                                        <div className="card-container" key={index}>
+                                        <div className="card-container" key={index} ref={cardContainerRef}>
                                             <div className="card">
                                                 <div className="img">
                                                     <a href={report.link} target="_blank" rel="noopener noreferrer">
@@ -93,12 +111,12 @@ export default function AnnualReport() {
                                     ))}
                                 </div>
                             </div>
-                            <div class="carousel-nav">
-                                <button class="prev hide">
-                                    <i class="fa fa-chevron-left"></i>
+                            <div className="carousel-nav">
+                                <button className="prev" ref={prevRef}>
+                                    <i className="fa fa-chevron-left"></i>
                                 </button>
-                                <button class="next">
-                                    <i class="fa fa-chevron-right"></i>
+                                <button className="next" ref={nextRef}>
+                                    <i className="fa fa-chevron-right"></i>
                                 </button>
                             </div>
                         </div>
@@ -106,5 +124,5 @@ export default function AnnualReport() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
