bf7df6d04564ba14b5dc5e9814ed7183a9d15410
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-06-22 16:43:27 +0530

    update home page carousel

--- ncc/src/components/Home/Home.js
+++ ncc/src/components/Home/Home.js
@@ -9,15 +9,19 @@ function Home() {
     <>
       <div id="homeCarousel" class="carousel slide carousel-home" data-bs-ride="carousel" >
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" class="active" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
         <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./images/home/carousel4.jpg" class="d-block w-100" style={{ marginTop: '0px' }} />
+          </div>
           <div class="carousel-item">
             <img src="./images/home/carousel1.jpg" class="d-block w-100" style={{ marginTop: '0px' }} />
           </div>
-          <div class="carousel-item active">
+          <div class="carousel-item">
             <img src="./images/home/carousel2.jpg" class="d-block w-100" style={{ marginTop: '0px' }} />
           </div>
           <div class="carousel-item">
