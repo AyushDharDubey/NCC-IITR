03cb10388b5d2b80d7e703387e9248a6668ed366
Mohit-7082 <mohit_t@me.iitr.ac.in>
2025-06-08 21:55:07 +0530

    code added

--- ncc/src/components/Events/Events.js
+++ ncc/src/components/Events/Events.js
@@ -10,6 +10,11 @@ function Events() {
 
     const events = [
         {
+            src: "./images/events/Firing25.jpeg",
+            alt: "Firing Practice at BEG Range",
+            dataBsTarget: "#exampleModalp36"
+        },
+        {
             src: "./images/events/guest-lec-col-anoop.jpg",
             alt: "Guest Lecture by Col. Anoop Vyas",
             dataBsTarget: "#exampleModalp35"
@@ -1196,6 +1201,34 @@ function Events() {
                                             <p className="event-details">
                                                 Venue - Chemical Auditorium, IIT Roorkee
                                             </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="modal fade" id="exampleModalp36" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-body p-0">
+                                <div className="container-fluid p-0">
+                                    <div className="row d-flex modal-box m-0">
+                                        <div className="col-6 px-0 align-self-center img-container">
+                                            <img
+                                                src="./images/events/firing-practice-2024.jpg"
+                                                alt="Firing Practice (in BEG) - 9th October"
+                                                className="d-block"
+                                                style={{ width: '100%', margin: '0px', padding: '0px', borderRadius: '0px' }}
+                                            />
+                                        </div>
+                                        <div className="col-6 modal-event">
+                                            <p className="text-center event-name" style={{ paddingBottom: '7px' }}>Firing Practice</p>
+                                            <p className="text-center event-des" style={{ padding: '0px 15px' }}>
+                                                Participate in an immersive firing practice session at BEG to improve your precision and control.
+                                            </p>
+                                            <p className="event-details">Date &amp; Time - 07-04-2025, 07:00 AM to 01:00 PM</p>
+                                            <p className="event-details">Venue - BEG Firing Range</p>
                                         </div>
                                     </div>
                                 </div>
